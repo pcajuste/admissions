fbea84a87ba2a4a1ba92aa61faeb302278851102
Nathai <spontandom@gmail.com>
2015-10-06 17:25:18 -0400

    added bootstrap nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    render component: 'Test', props: {users: @users}
   end
 
   # GET /users/1
