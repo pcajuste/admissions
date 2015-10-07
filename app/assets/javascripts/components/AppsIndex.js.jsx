8d5d7e4088999926bee328dacf29ea5048605f5a
moelgendy <mohamed@elgendyconsulting.com>
2015-10-07 14:40:30 -0400

    fixed the AppsIndex component

--- app/assets/javascripts/components/AppsIndex.js.jsx
+++ app/assets/javascripts/components/AppsIndex.js.jsx
@@ -1,44 +1,44 @@
 var AppsIndex = React.createClass({
   render: function() {
     var appNodes = this.props.applications.map(function(app){
-          return (
-          <div>
-          <table>
+        return (
+          <div className="row">
+            <div className="col-md-1">{app.user_id}</div>
+            <div className="col-md-1">{app.course_id}</div>
+            <div className="col-md-1">{app.resume}</div>
+            <div className="col-md-1">{app.education}</div>
+            <div className="col-md-1">{app.experience}</div>
+            <div className="col-md-1">{app.skils}</div>
+            <div className="col-md-1">{app.notes}</div>
+          </div>
+        )
 
-                <tbody>
-                  <tr>
-                    <td>{app.education}</td>
-                    <td>{app.experience}</td>
-                    <td>{app.status}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          )
     });
 
       return(
         <div>
-            <div className="container">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Education</th>
-                    <th>Experience</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                  <td>{appNodes}</td>
-                  <td>{appNodes}</td>
-                  <td>{appNodes}</td>
-
-              </table>
-            </div>
+        <div className="row">
+          <strong><div className="col-md-1">User ID</div></strong>
+          <strong><div className="col-md-1">Course ID</div></strong>
+          <strong><div className="col-md-1">Resume</div></strong>
+          <strong><div className="col-md-1">Education</div></strong>
+          <strong><div className="col-md-1">Experience</div></strong>
+          <strong><div className="col-md-1">Skills</div></strong>
+          <strong><div className="col-md-1">Comments</div></strong>
         </div>
+      <div>{appNodes}</div>
+      </div>
     )
-    console.log(appNodes);
   }
 });
+
+
+
+
+
+
+
+
 var LikeButton = React.createClass({
   getInitialState: function() {
     return {liked: false};
@@ -55,10 +55,3 @@ var LikeButton = React.createClass({
     );
   }
 });
-// var IndexTable = React.createClass({
-//   render: function(){
-//     return(
-//
-//     )
-//   }
-// });
