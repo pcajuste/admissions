6bc85f6dd8e15e05c5a161f79b40d35dcfaa31fc
moelgendy <mohamed@elgendyconsulting.com>
2015-10-07 01:31:25 -0400

    added the AppsIndex component

--- app/assets/javascripts/components/AppsIndex.js.jsx
+++ app/assets/javascripts/components/AppsIndex.js.jsx
@@ -19,12 +19,30 @@ var AppsIndex = React.createClass({
                   </tr>
                 </tbody>
               </table>
+              <h2><LikeButton/></h2>
             </div>
           )
     });
       return(
         <div>{appNodes}</div>
     )
+  }
+});
+
+var LikeButton = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'Please submit your application' : 'Cancel you application';
+    return (
+      <h3 onClick={this.handleClick}>
+        {text}. Click here!
+      </h3>
+    );
   }
 });
 // var IndexTable = React.createClass({
